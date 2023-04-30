b568cb98e62907aac6d4b2d07fa26c470d5e7efc
mao-tk <126842098+mao-tk@users.noreply.github.com>
2023-04-30 01:43:34 +0000

    [Finish]Swiper_app

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -8,3 +8,18 @@ $(function(){
     $(this).css('color', 'red');
   });
 });
+
+const opt = {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+}
+
+$(document).on('turbolinks:load', function() {
+  let swiper = new Swiper('.swiper',opt);
+});
